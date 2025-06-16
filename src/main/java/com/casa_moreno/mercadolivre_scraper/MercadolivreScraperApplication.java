22dae28dc7594cbee60c1a49b753e528eb89aae6
Matteus Moreno <matteus_moreno@live.com>
2025-06-16 11:50:16 -0300

    initial commit

---
+++ src/main/java/com/casa_moreno/mercadolivre_scraper/MercadolivreScraperApplication.java
@@ -0,0 +1,13 @@
+package com.casa_moreno.mercadolivre_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadolivreScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadolivreScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casa_moreno/mercadolivre_scraper/MercadolivreScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casa_moreno.mercadolivre_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
